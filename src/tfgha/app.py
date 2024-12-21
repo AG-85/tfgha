2809efe7f3b81265f89d3ab5994d99b85c8e1966
Andy Miles <akmiles@icloud.com>
2024-12-20 19:39:56 -0800

    chg message

--- src/tfgha/app.py
+++ src/tfgha/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Python deployed with poetry"
+    return "Hello from Python deployed with poetry modified"
 
 
 if __name__ == '__main__':
